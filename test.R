72f0c0d17547656e05dec65c117ef61a19b0aad6
Sana Iqbal <sna.iqbl@gmail.com>
2017-05-16 01:43:21 +0300

    Computed OLS estimates

--- test.R
+++ test.R
@@ -4,7 +4,8 @@ ytest <- data.frame(y = c(1,2,4,6,4,12,11,13,14,16,17))
 ytest.mat <- as.matrix(ytest)
 
 #OLS
-X <- data.frame(i = matrix(1,10,1),t=data$t)
-x.mat <- as.matrix(X)
-beta_ols <- (solve(t(x.mat)%*%x.mat))%*%(t(x.mat)%*%y)
+#create matrix of regressors where i is column of 1s and t is trend variable
+X <- data.frame(i = matrix(1,10,1),t=data$t) 
+x.mat <- as.matrix(X) #convert to matrix
+beta_ols <- (solve(t(x.mat)%*%x.mat))%*%(t(x.mat)%*%y) #compute ols estimates
 rownames(beta_ols) <- c("intercept", "t")
